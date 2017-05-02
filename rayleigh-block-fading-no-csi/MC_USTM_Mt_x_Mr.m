fc5029702c0c9c27036bc21ec0db7796acd117c1
thephuong <thephuong@gmail.com>
2017-05-02 12:50:26 +0200

    seperate the createM_log()

--- rayleigh-block-fading-no-csi/MC_USTM_Mt_x_Mr.m
+++ rayleigh-block-fading-no-csi/MC_USTM_Mt_x_Mr.m
@@ -62,7 +62,8 @@ for k=1:K
             Sigma=svd(D*Z(:,:,l)).^2;
             
             % Create matrix M, automatic
-            M = createM(Mtalt,Mr,T,Sigma,lambda2);
+%             M = createM(Mtalt,Mr,T,Sigma,lambda2);
+            M = createM_log(Mtalt,Mr,T,Sigma,lambda2);
             logdetM=log(det(M));
             vanderTerm = det(vander(Sigma)); %get the determinant of the vandermode matrix
             TraceZ=real(trace(Z(:,:,l)'*Z(:,:,l)));
@@ -208,26 +209,6 @@ function val = logComplexGammaRatio(Mt,Mr,T)
     if (Mt > 2)
         %because gammaln(1) = gammaln(2) = 0
         val = val + sum(gammaln(3:Mt));
-    end
-end
-
-%% TODO: need to optimize it, i.e. avoid exp(.)
-function M = createM(Mt,Mr,T,Sigma,lambda)
-    M = nan(Mr,Mr); %(l is the row, k is the column)
-    for l = 1:Mr,
-        for k=1:Mt,
-%             M(l,k)=(Sigma(l)^(Mt-k))*gammainc(lambda*Sigma(l),T+k-Mt-Mr);
-            tmp = (Mt-k)*log(Sigma(l)) + log(gammainc(lambda*Sigma(l),T+k-Mt-Mr));
-            M(l,k) = exp(tmp);
-        end
-        for k=Mt+1:Mr,
-%             M(l,k)=(Sigma(l)^(T-k))*exp(-Sigma(l)*lambda); % case Mt<Mr
-            tmp = (T-k)*log(Sigma(l)) - Sigma(l)*lambda;
-            M(l,k) = exp(tmp);
-        end
-    end
-    if (any(isnan(M(:))) || any(isinf(M(:))))
-        error('createM() number overflow');
     end
 end
 
